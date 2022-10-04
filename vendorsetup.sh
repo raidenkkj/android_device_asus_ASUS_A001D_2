346281735fb3db422a17ff48feae295667d80940
Raiden Ishigami <contact.raidenishi69@gmail.com>
2022-10-03 21:27:57 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_ASUS_A001D_2-user
-add_lunch_combo omni_ASUS_A001D_2-userdebug
-add_lunch_combo omni_ASUS_A001D_2-eng
+add_lunch_combo omni_A001D-user
+add_lunch_combo omni_A001D-userdebug
+add_lunch_combo omni_A001D-eng
